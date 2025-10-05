86b2796fe9a2b17ceaa7cf7a41ee15f28f13c8b3
adityism <adityism@192.168.1.23>
2025-10-05 22:23:33 +0530

    Refactor: use .env for secrets, update .gitignore, and requirements.txt

--- backend/src/test_models.py
+++ backend/src/test_models.py
@@ -2,8 +2,9 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
-genai.configure(api_key=os.getenv("AIzaSyAdXQlqtuDic-DeRmY0hGFd-472gJ2FjaA"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 for m in genai.list_models():
     print(m.name)
